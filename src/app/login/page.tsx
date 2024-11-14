dffcea9a1b634c2e698115edaa7a4c15fe5ef698
ao2395 <ao2395@nyu.edu>
2024-11-14 13:56:30 -0500

    quote

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -101,7 +101,7 @@ export default function Login() {
               </div>
               <div className="relative flex justify-center text-sm">
                 <span className="px-2 bg-white text-gray-500">
-                "Don&apos;t have an account?"
+                  Don't have an account?
                 </span>
               </div>
             </div>
